7fad35b4c4ae90065f89981bfa7be5e3b9ff6364
ntxm <51963319+ntxm@users.noreply.github.com>
2020-01-15 01:28:09 -0500

    Dashboard test added

--- src/HRMSystemTestNG/Dashboard.java
+++ src/HRMSystemTestNG/Dashboard.java
@@ -1,14 +1,43 @@
 package HRMSystemTestNG;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import com.utils.CommonMethods;
 import com.utils.Constants;
+
+/**
+ * ===============
+ * @author ntxm    
+ * ===============
+ * login() --> Open browser and navigate to HRMS
+ * 
+ * dashboard() -->  Check that correct page was opening. Verifying title.
+ * 
+ * quickLaunch() --> Check Quick Launch Block.
+ * 		Step 1: Check if "Quick Launch" block is displayed
+ * 		Step 2: Check if all icons from block is displayed
+ * 		Step 3: Check if text of links is correct
+ * 
+ * employeeDistribution() --> Check if block "Employee Distribution by Subunit" Block.
+ * 		Step 1: Verify if block is displayed.
+ * 		Step 2: Verify text displayed when you hold mouse over the element
+ * 
+ * screensShot() --> Takes screenshot
+ * 
+ * tearDown() --> Close the browser
+ */
 
 public class Dashboard extends CommonMethods {
 	
@@ -23,11 +52,6 @@ public class Dashboard extends CommonMethods {
 		
 	}
 	
-	@AfterClass
-	public void tearDown() {
-		driver.quit();
-	}
-	
 	
 	@Test (priority = 1, enabled = true, groups = "Dashboard")
 	//Title
@@ -39,13 +63,20 @@ public class Dashboard extends CommonMethods {
 		softAssert.assertAll();
 	}
 	
-	@Test(priority = 1, enabled = true, groups ="Dashboard")
+	@Test(priority = 2, enabled = true, groups ="Dashboard")
 	//Quick Launch
 	public void quickLaunch() {
 		//block displayed
 		boolean isQuickLaunch = driver.findElement(By.id("panel_resizable_0_0")).isDisplayed();
 		SoftAssert softAssert = new SoftAssert();
 		softAssert.assertTrue(isQuickLaunch, "ERROR: block does not displayed");
+		
+		//icons displayed
+		List<WebElement> icons = driver.findElements(By.xpath("//table[@class='quickLaungeContainer']/tbody/tr/td//div[@class='quickLaunge']/a/img"));
+		for(WebElement icon: icons) {
+			boolean isIconDisplayed = icon.isDisplayed();
+			softAssert.assertTrue(isIconDisplayed, "ERROR: icon not displayed");
+		}
 		
 		//-----------------------------------
 		//Link Text from Quick Launch Block
@@ -74,9 +105,37 @@ public class Dashboard extends CommonMethods {
 		
 	}
 	
+	@Test(priority = 3, enabled = true, groups ="Dashboard")
+	public void employeeDistribution() {
+		boolean isBlockDisplayed = driver.findElement(By.id("panel_resizable_1_0")).isDisplayed();
+		
+		SoftAssert softAssert = new SoftAssert();
+		softAssert.assertTrue(isBlockDisplayed, "ERROR: block does NOT displayed");
+		
+		WebElement diagramma = driver.findElement(By.xpath("//canvas[@class='flot-overlay']"));
+		Actions action = new Actions(driver);
+		action.moveToElement(diagramma).perform();
+		boolean diagrammaText = driver.findElement(By.xpath("//div[@id='hover_div_graph_display_emp_distribution']")).isDisplayed();
+		softAssert.assertTrue(diagrammaText, "ERROR: message is NOT displayed");
+		softAssert.assertAll();
+	}
+	
+	@AfterClass
+	public void screensShot() {
+		TakesScreenshot screen = (TakesScreenshot)driver;
+		File testResult = screen.getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(testResult, new File("screenshots/HRMS/Dashboard.jpeg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
-	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
 	
 	
 
